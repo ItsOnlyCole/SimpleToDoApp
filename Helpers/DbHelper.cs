b687e4377a9b161565d0bae0e5eb37a5ccd7f1bc
ItsOnlyCole <itsonlycole@gmail.com>
2021-01-26 13:50:18 -0500

    Create DbHelper.cs

---
+++ Helpers/DbHelper.cs
@@ -0,0 +1,53 @@
+using System.IO;
+using Microsoft.Data.Sqlite;
+
+namespace SimpleToDoApp.Helpers
+{
+    public class DbHelper
+    {
+        public SqliteConnectionStringBuilder GetConnection()
+        {
+            var connectionStringBuilder = new SqliteConnectionStringBuilder();
+            connectionStringBuilder.DataSource = "ToDoList.db";
+
+            return connectionStringBuilder;
+        }
+        
+        public bool CheckIfDbExist(SqliteConnectionStringBuilder databaseConnectionString)
+        {
+            if(File.Exists(databaseConnectionString.DataSource))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public void CreateDb()
+        {
+            using (var connection = new SqliteConnection(GetConnection().ConnectionString))
+            {
+                connection.Open();
+                connection.Close();
+            }
+        }
+        public void CreateTable()
+        {
+            using (var connection = new SqliteConnection(GetConnection().ConnectionString))
+            {
+                connection.Open();
+
+                var createTableCmd = connection.CreateCommand();
+                createTableCmd.CommandText = 
+                @"CREATE TABLE todo_items(
+                    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                    add_date DATETIME NOT NULL,
+                    title NVARCHAR(200) NOT NULL,
+                    is_done BIT NOT NULL DEFAULT 0
+                    )";
+                createTableCmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
